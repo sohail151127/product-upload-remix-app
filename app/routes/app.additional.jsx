71b44275ba88a4f948d4da5117a6a99f871c2df9
sohail151127 <60110179+sohail151127@users.noreply.github.com>
2024-08-06 19:11:33 +0500

    updated

--- app/routes/app.additional.jsx
+++ app/routes/app.additional.jsx
@@ -13,7 +13,7 @@ import { TitleBar } from "@shopify/app-bridge-react";
 export default function AdditionalPage() {
   return (
     <Page>
-      <TitleBar title="Additional page" />
+      <TitleBar title="Sohail" />
       <Layout>
         <Layout.Section>
           <Card>
